98fa80e6aec7bc6ef56af34e60c2821cb438b537
Naveedshafi <98814692+Naveedshafi@users.noreply.github.com>
2022-03-07 23:30:17 +0530

    Create lcd.h

---
+++ MINI PROJECT/3_IMPLEMENTATION/inc/lcd.h
@@ -0,0 +1,89 @@
+/* LCD AVR Library
+ *
+ * Copyright (C) 2015 Sergey Denisov.
+ * Written by Sergey Denisov aka LittleBuster (DenisovS21@gmail.com)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public Licence
+ * as published by the Free Software Foundation; either version 3
+ * of the Licence, or (at your option) any later version.
+ *
+ * Original library written by SkewPL, http://skew.tk
+ */
+
+#ifndef __LCD_DISPLAY__
+#define __LCD_DISPLAY__
+
+#include <avr/pgmspace.h>
+#include <stdint.h>
+
+/*
+ * LCD's port
+ */
+#define PORT_LCD PORTC
+#define DDR_LCD DDRC
+
+/*
+ * LCD's pins
+ */
+#define LCD_SCE PC6
+#define LCD_RST PC2
+#define LCD_DC PC3
+#define LCD_DIN PC4
+#define LCD_CLK PC5
+
+#define LCD_CONTRAST 0x40
+
+/*
+ * Must be called once before any other function, initializes display
+ */
+void lcd_init(void);
+
+/*
+ * Clear screen
+ */
+void lcd_clear(void);
+
+/**
+ * Power of display
+ * @lcd: lcd  struct
+ * @on: 1 - on; 0 - off;
+ */
+void lcd_power(uint8_t on);
+
+/**
+ * Set single pixel
+ * @x: horizontal pozition
+ * @y: vertical position
+ * @value: show/hide pixel
+ */
+void lcd_set_pixel(uint8_t x, uint8_t y, uint8_t value);
+
+/**
+ * Draw single char with 1-6 scale
+ * @code: char code
+ * @scale: size of char
+ */
+void lcd_write_char(char code, uint8_t scale);
+
+/**
+ * Draw string. Example: writeString("abc",3);
+ * @str: sending string
+ * @scale: size of text
+ */
+void lcd_write_string(const char *str, uint8_t scale);
+
+/**
+ * Set cursor position
+ * @x: horizontal position
+ * @y: vertical position
+ */
+void lcd_set_cursor(uint8_t x, uint8_t y);
+
+/*
+ * Render screen to display
+ */
+void lcd_render(void);
+
+
+#endif
